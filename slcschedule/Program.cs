6e844b2f37212502df9255a18abb0b1b4b329292
vdigi00 <vinced66@gmail.com>
2024-07-06 16:14:26 -0400

    Added unit tests

--- slcschedule/Program.cs
+++ slcschedule/Program.cs
@@ -14,33 +14,29 @@ namespace slcschedule
         {
             List<Employee> employees = new List<Employee>();
 
-            employees.Add(new Employee("John", "Doe", Employee.Position.Supervisor));
-            employees.Add(new Employee("Vincent", "DiCarlo", Employee.Position.Recreation));
-            employees.Add(new Employee("Billy", "Joel", Employee.Position.RSM));
-            foreach (var employee in employees)
-            {
-                Console.WriteLine(employee);
-                Console.WriteLine(employee.PrintEmployee());
-                //Console.WriteLine(employee.PrintAvailability())
-            }
-            /*
+            Employee Vincent1 = new Employee("Vincent", "DiCarlo", Employee.Position.Recreation);
+            Employee Billy1 = new Employee("Billy", "Joel", Employee.Position.RSM);
+            Employee John1 = new Employee("John", "Doe", Employee.Position.Supervisor);
+
+            employees.Add(Vincent1);
+            employees.Add(Billy1);
+            employees.Add(John1);
+
             John1.AddAvailability(10, 0, 14, 0);
             John1.AddAvailability(18, 0, 20, 0);
-            Console.WriteLine(John1.PrintEmployee());
-            Console.WriteLine(John1.PrintAvailability());
 
-            
             Vincent1.AddAvailability(8, 0, 12, 0);
             Vincent1.AddAvailability(15, 0, 20, 0);
-            Console.WriteLine(Vincent1.PrintEmployee());
-            Console.WriteLine(Vincent1.PrintAvailability());
 
-            
             Billy1.AddAvailability(5, 0, 12, 0);
             Billy1.AddAvailability(18, 0, 22, 0);
-            Console.WriteLine(Billy1.PrintEmployee());
-            Console.WriteLine(Billy1.PrintAvailability());
-            */
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.PrintEmployee());
+                Console.WriteLine(employee.PrintAvailability());
+            }
+            
             Console.WriteLine("Press any key to exit...");
             while (true)
             {
